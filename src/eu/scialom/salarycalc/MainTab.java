95ffd63a9226a41cb5bfa3a2f9955a54c816bb06
yscialom <yankel.scialom@mail.com>
2014-01-16 21:45:13 +0100

    Clean 'Calculator' UI.

--- src/eu/scialom/salarycalc/MainTab.java
+++ src/eu/scialom/salarycalc/MainTab.java
@@ -4,8 +4,10 @@ import java.util.Vector;
 
 import android.content.Context;
 import android.text.InputType;
+import android.view.Display;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -18,20 +20,36 @@ public class MainTab extends LinearLayout implements MyTab, OnClickListener {
 		public TextView name;
 		public EditText value;
 		public Button submit;
+		private final int displayWidth;
 
-		public Format(Context context, String name, String defaultValue) {
+		@SuppressWarnings("deprecation")
+		public Format(Context context, String name, String defaultValue, OnClickListener cb) {
 			super(context);
+
+			final WindowManager wm = (WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE);
+			final Display display = wm.getDefaultDisplay();
+			this.displayWidth = display.getWidth();
+
 			this.setOrientation(LinearLayout.HORIZONTAL);
 			this.name = new TextView(context);
 			this.name.setText(name);
+			this.name.setMinimumWidth(this.pixelWidthFromPerCent(20));
 			this.addView(this.name);
 			this.value = new EditText(context);
 			this.value.setInputType(InputType.TYPE_CLASS_NUMBER);
 			this.value.setText(defaultValue);
+			this.value.setMinimumWidth(this.pixelWidthFromPerCent(60));
 			this.addView(this.value);
 			this.submit = new Button(context);
 			this.submit.setText("Compute");
+			this.submit.setMinimumWidth(this.pixelWidthFromPerCent(20));
+			this.submit.setOnClickListener(cb);
 			this.addView(this.submit);
+		}
+
+		private int pixelWidthFromPerCent(int percentage) {
+			return percentage * this.displayWidth / 100;
 		}
 	}
 
@@ -42,12 +60,12 @@ public class MainTab extends LinearLayout implements MyTab, OnClickListener {
 
 		this.setOrientation(LinearLayout.VERTICAL);
 		this.formats = new Vector<Format>();
-		this.formats.add(new Format(this.getContext(), "Annual (BT):", "14400"));
-		this.formats.add(new Format(this.getContext(), "Annual (AT):", ""));
-		this.formats.add(new Format(this.getContext(), "Monthly (BT):", ""));
-		this.formats.add(new Format(this.getContext(), "Monthly (AT):", ""));
-		this.formats.add(new Format(this.getContext(), "Hourly (BT):", ""));
-		this.formats.add(new Format(this.getContext(), "Hourly (AT):", ""));
+		this.formats.add(new Format(this.getContext(), "Annual (BT):", "14400", this));
+		this.formats.add(new Format(this.getContext(), "Annual (AT):", "", this));
+		this.formats.add(new Format(this.getContext(), "Monthly (BT):", "", this));
+		this.formats.add(new Format(this.getContext(), "Monthly (AT):", "", this));
+		this.formats.add(new Format(this.getContext(), "Hourly (BT):", "", this));
+		this.formats.add(new Format(this.getContext(), "Hourly (AT):", "", this));
 
 		this.regenUI();
 	}
@@ -63,10 +81,7 @@ public class MainTab extends LinearLayout implements MyTab, OnClickListener {
 	}
 
 	@Override
-	public void onClick(View v) {/*
-								 * Button sender = (Button) v; for (Form f :
-								 * this.forms) { if (f.) }
-								 */
+	public void onClick(View v) {
 		Toast.makeText(this.getContext(), " ~~ LOL ~~ ", Toast.LENGTH_LONG).show();
 	}
 
