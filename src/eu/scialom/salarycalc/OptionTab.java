d8e158eea8b6db7554f395c0546e9ec6af15eca3
yscialom <yankel.scialom@mail.com>
2014-01-17 01:18:03 +0100

    Add (fake) option list.

--- src/eu/scialom/salarycalc/OptionTab.java
+++ src/eu/scialom/salarycalc/OptionTab.java
@@ -1,17 +1,70 @@
 package eu.scialom.salarycalc;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.RelativeLayout;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class OptionTab extends RelativeLayout implements MyTab {
+public class OptionTab extends ListView implements MyTab, OnItemClickListener {
+
+	public class Adapter extends ArrayAdapter<Option> {
+		private final Context context;
+		private final Option[] values;
+
+		public Adapter(Context context, Option[] values) {
+			super(context, R.layout.option_tab_item, values);
+			this.context = context;
+			this.values = values;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			final LayoutInflater inflater = (LayoutInflater) this.context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			final View rowView = inflater.inflate(R.layout.option_tab_item, parent, false);
+			final TextView name = (TextView) rowView.findViewById(R.id.name);
+			final TextView desc = (TextView) rowView.findViewById(R.id.description);
+			name.setText(this.values[position].name);
+			desc.setText(this.values[position].description);
+			return rowView;
+		}
+	}
+
+	public class Option {
+		public static final int TYPE_TEXT = 0x1;
+		public static final int TYPE_INTEGER = 0x2;
+		public static final int TYPE_FLOAT = 0x4;
+
+		public String name;
+		public String description;
+		public int type;
+		public Object value;
+
+		public Option(String name, String description, int type, Object value) {
+			this.name = name;
+			this.description = description;
+			this.type = type;
+			this.value = value;
+		}
+
+	}
 
 	public OptionTab(Context context) {
 		super(context);
-		final TextView t = new TextView(context);
-		t.setText("Bye!\n" + this.getShortName());
-		this.addView(t);
+
+		final Option opt[] = new Option[] {
+			new Option("TEXT", "This is a string", Option.TYPE_TEXT, "value"),
+			new Option("INTEGER", "This is an integer", Option.TYPE_INTEGER, 42),
+			new Option("FLOAT", "This is SPARTAAAAA!", Option.TYPE_FLOAT, -42.0f), };
+
+		final Adapter data = new Adapter(context, opt);
+		this.setAdapter(data);
+		this.setOnItemClickListener(this);
 	}
 
 	@Override
@@ -24,4 +77,7 @@ public class OptionTab extends RelativeLayout implements MyTab {
 		return this;
 	}
 
+	@Override
+	public void onItemClick(AdapterView<?> parent, View sender, int position, long lposition) {
+	}
 }
