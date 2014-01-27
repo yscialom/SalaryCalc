65e2af66239594a937291fa4d99589642df07f0e
yscialom <yankel.scialom@mail.com>
2014-01-27 18:25:10 +0100

    Add GPLv2 header to source files.
    Also added into HEADER.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 SalaryCalc
 ==========
 
-Android app to help users converting salary from different formats (€/y before taxes, €/m after taxes, etc.)
+Android app to help users convert salary from/to many formats.
 
 ScreenShots
 ==========
@@ -14,3 +14,24 @@ Options (french localization):
 
 ![JobCatcher Screenshot](https://raw.github.com/yscialom/SalaryCalc/master/screenshots/options_tab.png)
 
+License
+==========
+Copyright (C) 2014 Yankel Scialom.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Contacts
+==========
+Yankel Scialom (YSC) <yankel.scialom@mail.com>
--- gitv.sh
+++ gitv.sh
@@ -1,4 +1,25 @@
 #/bin/bash
+# 
+# SalaryCalc: android app to help users convert salary from/to many formats.
+# Copyright (C) 2014 Yankel Scialom.
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# 
+# \author Yankel Scialom (YSC) <yankel.scialom@mail.com>
+# \file ./src/eu/scialom/salarycalc/MainActivity.java
+# 
 
 echo "Auto Version: $(pwd)"
 
@@ -6,15 +27,15 @@ CODE=$(git tag | grep -c ^v[0-9])
 NAME=$(git describe --always --dirty | sed -e 's/^v//')
 COMMITS=$(echo ${NAME} | sed -e 's/[0-9\.]*//')
 
-if [ "x${COMMITS}x" = "xx" ] ; then
+if [ -z "${COMMITS}" ] ; then
     VERSION="${NAME}"
 else
     BRANCH=" ($(git branch | grep "^\*" | sed -e 's/^..//'))"
     VERSION="${NAME}${BRANCH}"
 fi
 
-echo "   Code: ${CODE}"
-echo "   Ver:  ${VERSION}"
+echo -e "\tCode:\t${CODE}"
+echo -e "\tVer:\t${VERSION}"
 
 sed -i -e "s/android:versionCode=\"[0-9][0-9]*\"/android:versionCode=\"${CODE}\"/" \
 	-e "s/android:versionName=\".*\"/android:versionName=\"${VERSION}\"/" \
